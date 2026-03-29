4486ee983af389ecbd78efb096bf0eb05f813f9c
LemmaGit <getiyelemma07@gmail.com>
2026-03-29 20:27:47 +0300

    changed the db config to use mongo atlas db instead of the local one

--- backend/src/lib/db.ts
+++ backend/src/lib/db.ts
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 export const connectDb = async () => {
 
-  // const mongoUri = process.env.MONGO_REMOTE_URI;
-  const mongoUri = "mongodb://127.0.0.1:27017/mitihealth-mvp";
+  const mongoUri = process.env.MONGO_REMOTE_URI;
+  // const mongoUri = "mongodb://127.0.0.1:27017/mitihealth-mvp";
 
   if (!mongoUri) {
     throw new Error("MONGODB_URI is not defined");
